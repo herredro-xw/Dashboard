26ec321e6772c59cd75dc44cded09c0de7af0b60
herredro-xw <lukas.herrero@kreuzwerker.de>
2020-03-16 18:06:02 +0100

    Updates

---
+++ precache-manifest.460ef718a26c67a615b44fbe34de3e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474f53013296a372dbb23f3115092848",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "1be4c87a9048fa26723e",
+    "url": "/Dashboard/static/css/main.b48d31d3.chunk.css"
+  },
+  {
+    "revision": "4bd9bc3f4893578b1629",
+    "url": "/Dashboard/static/js/2.77866572.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Dashboard/static/js/2.77866572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be4c87a9048fa26723e",
+    "url": "/Dashboard/static/js/main.9c9c075e.chunk.js"
+  },
+  {
+    "revision": "a4b47a6685bfe5b495d7",
+    "url": "/Dashboard/static/js/runtime-main.ef37a8a7.js"
+  }
+]);
